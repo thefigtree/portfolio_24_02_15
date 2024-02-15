6eb63e72f53d04b37b751e296e2e7cb475fa3e08
thefigtree <ldj8431@naver.com>
2024-02-15 22:03:15 +0900

    headerRef, scrollHandler

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import { useEffect, useRef } from "react";
+
+export const Header = () => {
+  const headerRef = useRef();
+
+  const scrollHandler = () => {};
+
+  useEffect(() => {});
+
+  return <div></div>;
+};
