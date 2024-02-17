0796fafb0cfda793f4e9a6afa60a602a4a9dda58
thefigtree <ldj8431@naver.com>
2024-02-18 02:34:57 +0900

    Section Design

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,6 +1,4 @@
-// import { useEffect, useRef } from "react";
-
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Section = styled.section`
   width: 100vw;
@@ -22,41 +20,70 @@ const TextContainer = styled.div`
   color: wheat;
 `;
 
+const moveUp = keyframes`
+100% {
+  transform: translateY(0);
+}
+`;
+
 const Text = styled.p`
   width: 50%;
-  font-size: 31px;
+  font-size: 21px;
   position: relative;
-  height: 80px;
+  height: 35px;
+  overflow: hidden;
   span {
-    background-color: lightblue;
     position: absolute;
+    transform: translateY(3rem);
+    animation-name: ${moveUp};
+    animation-duration: 2.5s;
+    animation-timing-function: ease;
+    animation-fill-mode: forwards;
+    animation-delay: ${(props) => props.delay};
+    background: linear-gradient(
+      -45deg,
+      #35c3f3 0%,
+      #8b9fe8 20%,
+      #e681d8 39%,
+      #ffa9a4 76%,
+      #fed2ce 100%
+    );
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+  .myname {
+    width: 100%;
+    text-align: end;
+    background-image: linear-gradient(
+      -180deg,
+      #35c3f3 0%,
+      #8b9fe8 20%,
+      #e681d8 39%,
+      #ffa9a4 76%,
+      #fed2ce 100%
+    );
   }
 `;
 
 export const Quote = () => {
-  // const headerRef = useRef();
-
-  // const scrollHandler = () => {};
-
-  // useEffect(() => {});
-
   return (
     <Section>
       <TextContainer>
-        <Text>
+        <Text delay="0">
           <span>line-1</span>
         </Text>
-        <Text>
+        <Text delay="0.4s">
           <span>line-2</span>
         </Text>
-        <Text>
+        <Text delay="0.8s">
           <span>line-3</span>
         </Text>
-        <Text>
+        <Text delay="1.2s">
           <span>line-4</span>
         </Text>
-        <Text>
-          <span>line-5</span>
+        <Text delay="1.6s">
+          <span className="myname">- 이동진</span>
         </Text>
       </TextContainer>
     </Section>
