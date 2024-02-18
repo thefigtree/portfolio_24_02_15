b0645fd8df88a3b440a81cbed90e86aa57b7a6ef
thefigtree <ldj8431@naver.com>
2024-02-18 15:31:43 +0900

    Color Change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { Quote } from "./components/Quote";
+// import { Quote } from "./components/Quote";
+import { ColorSection } from "./sections/ColorSection";
 
 const App = () => {
   return (
     <>
-      <Quote></Quote>
+      {/* <Quote></Quote> */}
+      <ColorSection></ColorSection>
     </>
   );
 };
