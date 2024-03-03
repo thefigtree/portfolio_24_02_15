0b36529da70638a5578bfb22659a6ec3948857a3
thefigtree <ldj8431@naver.com>
2024-03-04 00:05:59 +0900

    Header Figure

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,8 @@ import styled from "styled-components";
 const SHeader = styled.header`
   width: 100%;
   padding: 30px 5%;
-  /* height: 100px; */
-  /* background-color: white; */
-  /* position: relative; */
-  border-bottom: 1px solid plum;
-  /* position: absolute; */
-  /* top: 0; */
-  /* left: 0; */
+  background-color: #111;
+  border-bottom: 1px solid #ff0098;
 `;
 
 export const Header = () => {
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,10 @@
 import { SyntacticSugar } from "./SyntacticSugar";
 // import { ColorSection } from "./ColorSection";
 import { StackSection } from "./StackSection";
-import { Header } from "../../components/Header";
 
 export const Home = () => {
   return (
     <>
-      <Header></Header>
       <SyntacticSugar></SyntacticSugar>
       {/* <ColorSection></ColorSection> */}
       <StackSection></StackSection>
