ec2d753427e1703b54a851587a46083714519d02
thefigtree <ldj8431@naver.com>
2024-03-03 23:59:31 +0900

    Header Design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,17 @@
+import styled from "styled-components";
+
+const SHeader = styled.header`
+  width: 100%;
+  padding: 30px 5%;
+  /* height: 100px; */
+  /* background-color: white; */
+  /* position: relative; */
+  border-bottom: 1px solid plum;
+  /* position: absolute; */
+  /* top: 0; */
+  /* left: 0; */
+`;
+
 export const Header = () => {
-  return <div></div>;
+  return <SHeader></SHeader>;
 };
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,12 @@
 import { SyntacticSugar } from "./SyntacticSugar";
 // import { ColorSection } from "./ColorSection";
 import { StackSection } from "./StackSection";
+import { Header } from "../../components/Header";
 
 export const Home = () => {
   return (
     <>
+      <Header></Header>
       <SyntacticSugar></SyntacticSugar>
       {/* <ColorSection></ColorSection> */}
       <StackSection></StackSection>
