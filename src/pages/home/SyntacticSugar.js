869cadbe3bb55631667538312e92546724a8b933
thefigtree <ldj8431@naver.com>
2024-03-06 16:30:29 +0900

    Text Ani

--- src/pages/home/SyntacticSugar.js
+++ src/pages/home/SyntacticSugar.js
@@ -1,3 +1,7 @@
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+import { useLayoutEffect, useRef } from "react";
+import SplitType from "split-type";
 import styled from "styled-components";
 
 const Section = styled.section`
@@ -16,7 +20,7 @@ const TextCon = styled.h2`
   font-weight: 400;
   font-style: normal;
   letter-spacing: 3px;
-  color: gray;
+  color: #0e0e0e;
 `;
 
 const Stick = styled.div`
@@ -28,9 +32,56 @@ const Stick = styled.div`
 `;
 
 export const SyntacticSugar = () => {
+  const colorRef = useRef(null);
+  const COLORS_ARRAY = [
+    "#f25b40",
+    "#2c3d71",
+    "#e92d65",
+    "#f7b1cf",
+    "#86c1f7",
+    "#ffd570",
+    "#0e0e0e",
+  ];
+  const STEP_DURATION = 0.1;
+
+  useLayoutEffect(() => {
+    let Elem = colorRef.current;
+
+    gsap.set(Elem, { autoAlpha: 1 });
+
+    const SplitText = new SplitType(Elem, { types: "chars" });
+
+    gsap.set(SplitText.chars, { color: COLORS_ARRAY[0] });
+    gsap.registerEffect({
+      name: "changeColor",
+      effect: (targets, config) => {
+        return gsap.set(targets, {
+          delay: config.duration,
+          color: config.color,
+        });
+      },
+      default: { duration: STEP_DURATION },
+      extendTimeline: true,
+    });
+    gsap.from(SplitText.chars, {
+      scale: 0,
+      stagger: STEP_DURATION,
+      ease: "back.out",
+      color: (index, targets) => {
+        const t1Colors = gsap.timeline();
+        COLORS_ARRAY.forEach((color) => {
+          t1Colors.changeColor(targets, {
+            duration: STEP_DURATION,
+            color: color,
+          });
+        });
+      },
+    });
+  });
+
   return (
     <Section>
-      <TextCon>SyntacticSugar.</TextCon>
+      <TextCon ref={colorRef}>SyntacticSugar.</TextCon>
       <Stick></Stick>
     </Section>
   );
